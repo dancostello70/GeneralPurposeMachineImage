e93f463d95cbc67676fba3318439f0e08e169acf
Dan Costello <dancos@gmail.com>
2020-01-22 14:20:38 -0500

    Updated gitignore

--- Setup/XSPLK2/DoSetup.cmd
+++ Setup/XSPLK2/DoSetup.cmd
@@ -8,7 +8,6 @@ echo Beginning setup of XSPLK2
 echo Extracting archives
 powershell -ExecutionPolicy Bypass -File %SETUPDIR%\ExtractArchives.ps1
 
-
 :: Copy Desktop Files
 echo Copying desktop files
 xcopy /Y %SETUPDIR%\Desktop\*.* %USERPROFILE%\Desktop\
@@ -21,3 +20,13 @@ msiexec.exe /I splunk-8.0.1-6db836e2fb9e-x64-release.msi SPLUNKUSERNAME=admin SP
 
 echo Installing Java JRE
 jre-8u231-windows-x64.exe /s
+
+:: Set CURL path
+:: Actually gonna comment this out for now...
+:: echo Setting CURL path
+:: powershell -ExecutionPolicy Bypass -File %SETUPDIR%\SetCURLPath.ps1
+
+:: Update from github
+echo Updating from github
+powershell -ExecutionPolicy Bypass -File %SETUPDIR%\UpdateXSPLK2FromGithub.ps1
+
---
+++ Setup/XSPLK2/SetCURLPath.ps1
@@ -0,0 +1,5 @@
+﻿$curlPath = "C:\Program Files\curl-7.68.0-win64-mingw\bin"
+
+$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+$newpath = "$oldpath;$curlPath"
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
