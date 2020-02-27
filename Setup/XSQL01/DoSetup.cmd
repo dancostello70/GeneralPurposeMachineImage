93a8c638af981b786afce0bc2f6c10f47984d9f0
Dan-ONLC <danc@onlc.com>
2020-02-27 08:45:59 -0500

    Update DoSetup.cmd

--- Setup/XSQL01/DoSetup.cmd
+++ Setup/XSQL01/DoSetup.cmd
@@ -29,18 +29,18 @@ echo Extracting archives
 powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\ExtractArchives.ps1 -ClassId %CLASSID%
 
 :: Update from GitHub
-echo Updating from GitHub
-powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\UpdateClassfilesFromGithub.ps1 -ClassId %CLASSID%
+:: echo Updating from GitHub
+:: powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\UpdateClassfilesFromGithub.ps1 -ClassId %CLASSID%
 
 :: Copy Desktop Files
 echo Copying desktop files
 xcopy /Y %SETUPDIR%\Desktop\*.* %USERPROFILE%\Desktop\
 
 :: Do installations
-cd %SETUPDIR%\Installers
+:: cd %SETUPDIR%\Installers
 
-echo Installing __Application__
-__ApplicationSpecificInstallerCommandLine__
+:: echo Installing __Application__
+:: __ApplicationSpecificInstallerCommandLine__
 
 :: Install databases
 echo Setting up SQL Server databases
