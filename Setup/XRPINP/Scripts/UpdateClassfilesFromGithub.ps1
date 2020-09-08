417f02762cad840500ede9a113e505bdbefa64dc
Dan <dan@costellotech.com>
2020-09-08 02:05:04 -0400

    Created XRPINP scaffold

---
+++ Setup/XRPINP/Scripts/UpdateClassfilesFromGithub.ps1
@@ -0,0 +1,17 @@
+﻿# Update class files for __CLASSID__ (__ClassName__)
+param($ClassId)
+
+$selectedClassId = $ClassId
+
+$repoName = "__RepoName__"
+$ArchiveURI = "https://github.com/CostelloTechnicalConsulting/$repoName/archive/master.zip"
+$targetDir = "C:\$selectedClassIdClassFiles\"
+
+Invoke-WebRequest -UseBasicParsing -Uri $ArchiveURI -OutFile $env:TEMP\repo_temp.zip
+
+Expand-Archive -Force -Path $env:TEMP\repo_temp.zip -DestinationPath $env:TEMP\
+
+xcopy /c /e /y $env:TEMP\$repoName-master\* $targetDir
+
+Remove-Item -Force -Recurse $env:TEMP\repo_temp.zip
+Remove-Item -Force -Recurse $env:TEMP\$repoName-master
