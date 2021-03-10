5febace879ba13ff7f4253e0982be23a7f8e411a
Dan Costello <dancos@gmail.com>
2021-03-10 16:27:47 -0500

    Update MakeClassArchives.ps1

--- MakeClassArchives.ps1
+++ MakeClassArchives.ps1
@@ -8,6 +8,16 @@ $env:AZCOPY_CRED_TYPE = "Anonymous";
 
 if($true){
 
+$classId = "ADA100"
+
+    Compress-Archive -Force D:\DC\Clients\ONLC\Projects\CustomTraining\20200210BrooksourceDP200\AzureDataClassFiles\* -DestinationPath D:\DC\Clients\ONLC\Projects\CustomTraining\20200210BrooksourceDP200\AzureDataClassFiles.zip
+    # Upload
+    C:/Utility/azcopy.exe copy "D:\DC\Clients\ONLC\Projects\CustomTraining\20200210BrooksourceDP200\AzureDataClassFiles.zip" "https://onlcsetup.blob.core.windows.net/setupfiles/AzureDataClassFiles.zip?sv=2019-12-12&se=2021-03-14T01%3A57%3A11Z&sr=c&sp=rwl&sig=C%2B2FxB32w6xRsxoNdV%2Bug17NKdqCG%2BJSGwuvvXjFOeI%3D" --overwrite=true --from-to=LocalBlob --blob-type Detect --follow-symlinks --put-md5 --follow-symlinks --recursive;
+}
+
+
+if($false){
+
 $classId = "CS_DP200"
 
     Compress-Archive -Force D:\DC\Clients\ONLC\Projects\CustomTraining\20200210BrooksourceDP200\AzureDataClassFiles\* -DestinationPath D:\DC\Clients\ONLC\Projects\CustomTraining\20200210BrooksourceDP200\AzureDataClassFiles.zip
