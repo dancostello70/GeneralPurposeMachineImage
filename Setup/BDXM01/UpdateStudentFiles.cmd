daeacd9c322f071fa46a8b96c114a8ee29a1c068
Dan Costello <dancos@gmail.com>
2021-04-04 23:42:10 -0400

    Update UpdateStudentFiles.cmd

--- Setup/BDXM01/UpdateStudentFiles.cmd
+++ Setup/BDXM01/UpdateStudentFiles.cmd
@@ -2,4 +2,5 @@
 IF "%SETUPROOT%"=="" SET SETUPROOT=C:\Setup
 SET SETUPDIR=%SETUPROOT%\BDXM01
 
-powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/C_BDXM01ClassFiles.zip -localDownloadPath %TEMP%\C_BDXM01ClassFiles.zip -extractTargetPath C:\BDXM01ClassFiles
+powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01LabMaterials.zip -localDownloadPath %TEMP%\BDXM01LabMaterials.zip -extractTargetPath C:\Users\Student\Desktop\BDXM01LabMaterials
+powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01Data.zip -localDownloadPath %TEMP%\BDXM01Data.zip -extractTargetPath D:\Data
