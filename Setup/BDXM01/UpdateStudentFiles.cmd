4f6b6bda798c1ac21ad73c35a80bb6647462d79a
Dan Costello <dancos@gmail.com>
2021-04-05 15:00:33 -0400

    Add files to BDXM01

--- Setup/BDXM01/UpdateStudentFiles.cmd
+++ Setup/BDXM01/UpdateStudentFiles.cmd
@@ -3,4 +3,5 @@ IF "%SETUPROOT%"=="" SET SETUPROOT=C:\Setup
 SET SETUPDIR=%SETUPROOT%\BDXM01
 
 powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01LabMaterials.zip -localDownloadPath %TEMP%\BDXM01LabMaterials.zip -extractTargetPath C:\Users\Student\Desktop\BDXM01LabMaterials
-powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01Data.zip -localDownloadPath %TEMP%\BDXM01Data.zip -extractTargetPath D:\Data
+powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01_PBI.zip -localDownloadPath %TEMP%\BDXM01_PBI.zip -extractTargetPath C:\Users\Student\Desktop\BDXM01_PBI
+powershell -ExecutionPolicy Bypass -File %~dp0\Scripts\DownloadAndExtract.ps1 -URL https://onlcsetup.blob.core.windows.net/setupfiles/BDXM01_Data.zip -localDownloadPath %TEMP%\BDXM01_Data.zip -extractTargetPath D:\Data
