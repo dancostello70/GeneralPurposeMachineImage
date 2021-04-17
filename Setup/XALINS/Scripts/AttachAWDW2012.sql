86afe4a3ea61a70c22c976e9f77196442ddb6455
Dan Costello <dancos@gmail.com>
2021-04-16 23:55:06 -0400

    Update AttachAWDW2012.sql

--- Setup/XALINS/Scripts/AttachAWDW2012.sql
+++ Setup/XALINS/Scripts/AttachAWDW2012.sql
@@ -1,7 +1,6 @@
 USE [master]
 GO
 CREATE DATABASE [AdventureWorksDW2012] ON 
-( FILENAME = N'C:\XAL2NSClassFiles\Chapter-11\AdventureWorksDW2012_Data.mdf' ),
-( FILENAME = N'C:\XAL2NSClassFiles\Chapter-11\AdventureWorksDW2012_log.ldf' )
+( FILENAME = N'C:\XAL2NSClassFiles\Chapter-11\AdventureWorksDW2012_Data.mdf' )
  FOR ATTACH
 GO
