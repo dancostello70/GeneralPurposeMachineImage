1d19eb085bb5c797e7d2c8cd00179a6be54b894c
Dan Costello <dancos@gmail.com>
2022-01-05 13:35:47 -0500

    New BPBINT

--- Setup/BPBINT/DoSetup.cmd
+++ Setup/BPBINT/DoSetup.cmd
@@ -1,23 +1,35 @@
 @echo off
+:: Setup file for BPBINT (Power BI Intermediate)
+:: Version 1.0
+:: Last update: 2022-01-05
+:: Author: Dan Costello (dan@costellotech.com)
 
-SET SETUPDIR=%SETUPROOT%\BPBINT
+IF "%SETUPROOT%"=="" SET SETUPROOT=C:\Setup
+SET CLASSID=BPBINT
+SET SETUPDIR=%SETUPROOT%\%CLASSID%
+SET LOGFILE=%SETUPDIR%\SetupLog.log
+SET CLASSDIR=C:\%CLASSID%ClassFiles
 
-echo Beginning setup of BPBINT
 
-:: Download BI Data
-echo Downloading BI Data
-powershell -ExecutionPolicy Bypass -File %SETUPDIR%\DownloadFile.ps1 -fileUrl "https://onlcsetup.blob.core.windows.net/setupfiles/BIData.zip" -destinationPath  %SETUPDIR%\Archives\BIData.zip"
+:: Start log
+echo Beginning setup > %LOGFILE%
+date /t >> %LOGFILE%
+time /t >> %LOGFILE%
 
-:: Extract archives
-echo Extracting archives
-powershell -ExecutionPolicy Bypass -File %SETUPDIR%\ExtractArchives.ps1
+echo Beginning setup of %CLASSID%
+
+powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\DownloadExtractInstall.ps1 -SetupRoot %SETUPROOT% -ClassSetupDir %CLASSID%
+
+echo Download setup install complete >> %LOGFILE%
+time /t >> %LOGFILE%
 
 :: Copy Desktop Files
 echo Copying desktop files
 xcopy /Y %SETUPDIR%\Desktop\*.* %USERPROFILE%\Desktop\
 
-:: Install Power BI Desktop
-echo Installing Power BI Desktop
-%SETUPDIR%\PBIDesktopSetup_x64.exe -q ACCEPT_EULA=1
+:: Send an alert
+powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\SendAlert.ps1 -ClassId %CLASSID%
 
-echo Setup complete
+:: Log process complete
+echo Setup completed >> %LOGFILE%
+time /t >> %LOGFILE%
