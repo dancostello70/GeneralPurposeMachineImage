cd75d46a5ec5c975c5871793f5df89f7240b9bf1
Dan Costello <dancos@gmail.com>
2022-03-08 12:39:03 -0500

    Initialized APL300 Setup

---
+++ Setup/APL300/RemoveSetup.cmd
@@ -0,0 +1,35 @@
+::@echo off
+:: Uninstall file for APL300 (PL-300: Microsoft Power BI Data Analyst)
+:: Version 1.0
+:: Last update: __________
+:: Author: Dan Costello (dan@costellotech.com)
+
+IF "%SETUPROOT%"=="" SET SETUPROOT=C:\Setup
+SET CLASSID=APL300
+SET SETUPDIR=%SETUPROOT%\%CLASSID%
+SET LOGFILE=%SETUPDIR%\%CLASSID%_UninstallLog.log
+SET CLASSDIR=C:\Coursefiles\DAXIntro
+
+:: Start log
+echo Beginning uninstall > %LOGFILE%
+date /t >> %LOGFILE%
+time /t >> %LOGFILE%
+
+:: Do uninstalls
+
+powershell -ExecutionPolicy Bypass -File %SETUPDIR%\Scripts\UninstallAll.ps1 -SetupRoot %SETUPROOT% -ClassSetupDir %CLASSID%
+
+:: Delete stuff
+cd %SETUPROOT%
+rmdir /s /q %SETUPDIR%
+del %SETUPROOT%\%CLASSID%.zip
+
+del "%USERPROFILE%\Desktop\DAX Intro Files.lnk"
+
+del /f /s /q %CLASSDIR%\*.*
+rmdir /s /q %CLASSDIR%
+
+
+:: Log process complete
+echo Uninstall completed >> %LOGFILE%
+time /t >> %LOGFILE%
