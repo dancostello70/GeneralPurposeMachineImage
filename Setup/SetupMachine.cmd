4f034b2fda59442b72aae78f02a5892ac6e6d9ae
Dan Costello <dan@costellotech.com>
2023-01-22 13:30:15 -0500

    Update SetupMachine.cmd

--- Setup/SetupMachine.cmd
+++ Setup/SetupMachine.cmd
@@ -1,6 +1,10 @@
+:: Updated 2023-01-22
+:: dan@costellotech.com
+
 :: Debugging
 SET DEBUG=0
-IF NOT "%SETUP_DEBUG%"=="" IF NOT "%SETUP_DEBUG%"=="0" SET DEBUG=1
+IF NOT "%SETUP_DEBUG%"=="" (IF NOT "%SETUP_DEBUG%"=="0" (SET DEBUG=1))
+IF "%SETUPROOT%"=="" (SET SETUPROOT=%~dp0)
 
 powershell -ExecutionPolicy Bypass -File %SETUPROOT%\InitSetup.ps1
 
